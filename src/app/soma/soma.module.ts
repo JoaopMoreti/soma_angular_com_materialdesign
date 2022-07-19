0bca4188db6a5c9b13bbbbdb482955b870efc3cf
JaoMoreti <joao.moreti@ekaizen.digital>
2022-07-19 10:49:48 -0300

    commit1

---
+++ src/app/soma/soma.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SomaComponent } from './soma/soma.component';
+import { SomaService } from './services';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [
+    SomaComponent
+  ],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  exports:[
+    SomaComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  providers:[
+    SomaService
+  ]
+})
+export class SomaModule { }
